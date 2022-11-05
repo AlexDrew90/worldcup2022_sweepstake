5df7c989b1af66efffa914d6c7f70e530fa11ce3
Alex Drew <alexander.william.drew@gmail.com>
2022-11-06 03:44:08 +0800

    Added random selection of country

--- script.js
+++ script.js
@@ -4,17 +4,34 @@ const clearButton = document.querySelector(".clear-button");
 const submitButton = document.querySelector(".primary-cta");
 const outputList = document.querySelector(".add-names");
 let countriesArray = ["Argentina", "Australia", "Belgium", "Brazil", "Cameroon", "Canada", "Costa Rica", "Croatia", "Denmark", "Ecuador", "England", "France", "Germany", "Ghana", "Iran", "Japan", "Mexico", "Morocco", "Netherlands", "Poland", "Portugal", "Qatar", "Saudi Arabia", "Senegal", "Serbia", "South Korea", "Spain", "Switzerland", "Tunisia", "United States", "Uruguay", "Wales"];
+let namesArray = [];
 
 addButton.addEventListener ("click", (event) => {
-if (outputList.childElementCount < 2){
+  event.preventDefault()
+if (outputList.childElementCount < 32){
 const addedPerson = nameField.value;
+namesArray.push(addedPerson);
 const inputtedLi = `<li class="list-inline-item">${addedPerson}</li>`;
 outputList.insertAdjacentHTML("beforeend", inputtedLi);
 nameField.value ="";
-} else {alert("32 is the maximum. No more teams left!");
-return false;}
+} else {
+  alert("32 is the maximum. No more teams left!");
+  nameField.value ="";
+}
+return namesArray
 });
 
 clearButton.addEventListener ("click", (event) => {
   outputList.innerHTML="";
   });
+
+submitButton.addEventListener ("click", (event) => {
+  if (outputList.childElementCount > 1){
+    countriesArray.forEach((event) => {
+    const randCountry = countriesArray[Math.floor(Math.random()*countriesArray.length)];
+
+    })
+  }else {
+    alert("You need at least two players!");
+  }
+    });
